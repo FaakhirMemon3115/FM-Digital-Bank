fac3498cd218b60aee755fbe51fc7b01d5b03bee
FaakhirMemon030 <alexwatson031105@gmail.com>
2026-06-20 19:36:46 +0500

    feat: configure App module with database and environment variable providers

--- backend/dist/app.module.js
+++ backend/dist/app.module.js
@@ -26,7 +26,7 @@ exports.AppModule = AppModule = __decorate([
             typeorm_1.TypeOrmModule.forRoot({
                 type: 'mysql',
                 host: process.env.DB_HOST || 'localhost',
-                port: parseInt(process.env.DB_PORT) || 3306,
+                port: parseInt(process.env.DB_PORT || '3306', 10),
                 username: process.env.DB_USERNAME || 'root',
                 password: process.env.DB_PASSWORD || '',
                 database: process.env.DB_DATABASE || 'fm_digital_bank',
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,7 +14,7 @@ import { Transaction } from './modules/transactions/entities/transaction.entity'
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 3306,
+      port: parseInt(process.env.DB_PORT || '3306', 10),
       username: process.env.DB_USERNAME || 'root',
       password: process.env.DB_PASSWORD || '',
       database: process.env.DB_DATABASE || 'fm_digital_bank',
